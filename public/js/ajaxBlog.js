13b41cdb839a97895d62607300987edb90a9bbd8
sofyann <sofyann.fahim@gmail.com>
2018-04-09 12:34:21 +0200

    taxref & comments blog

--- public/js/ajaxBlog.js
+++ public/js/ajaxBlog.js
@@ -1,20 +1,20 @@
 $(function () {
     $(".submitButton").on('click', function () {
         var url = $(this).attr('data-url');
-        var content = $("textarea#comment_article_content").val();
+        var content = $("textarea#comment_article_content");
         var idArticle = $(this).attr('id-article');
-        console.log(content);
-        var data = { content : content,
+        var data = { content : content.val(),
             idArticle : idArticle
         };
-        console.log(data);
+        var idnewComment;
         $.ajax({
             url : url,
             type: 'POST',
             data : data
             ,
             success: function (response) {
-                console.log('modification effectuée avec succès '+ response);
+                console.log('modification effectuée avec succès '+ response.id);
+                idnewComment = response.id;
             },
             error: function () {
                 console.log('erreur lors de la requête')
@@ -25,12 +25,14 @@ $(function () {
                 .replace(/__nameUser__/g, 'Jérôme Butel')
                 .replace(/__date__/g, '30/03/2018')
                 .replace(/__hour__/g, '10h30')
-                .replace(/__comment__/g, content);
+                .replace(/__comment__/g, content.val())
+                .replace(/__id__/g, idnewComment);
             $containerComments.append(template);
+            content.val('');
         });
     });
 
-    $(".deleteButton").on("click", function () {
+    $("#comments").on("click", ".deleteButton", function () {
         var el = $(this).closest('.comment');
         var url = $(this).attr('data-url');
         $.ajax({
---
+++ src/Command/CsvImportCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CsvImportCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('csv:import')
+            ->setDescription('Import CSV file')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title('Attempting to import the feed...');
+
+
+
+        $io->success('Everything went well !');
+    }
+}
