992e6c4c969b05d53d84b9c772247c62ec6f6452
ajie62 <buteljerome@yahoo.fr>
2018-04-16 16:43:05 +0200

    User profile

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -39,67 +39,6 @@ class AppController extends AbstractController
     }
 
     /**
-     * Register page
-     * @Route("/register", name="app.register")
-     *
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        if ($this->getUser())
-            return $this->redirectToRoute('app.homepage');
-
-        $user = new User();
-
-        $form = $this->createForm(RegistrationType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // dump($form->getData()); die;
-
-            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
-            $this->em->persist($user);
-            $this->em->flush();
-
-            /** Send welcome email */
-            /** Add flash message */
-
-            return $this->redirectToRoute('app.homepage');
-        }
-
-        return $this->render('app/registration.html.twig', [
-            'registration_form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * Login page
-     * @Route("/login", name="app.login")
-     *
-     * @param AuthenticationUtils $utils
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
-    public function login(AuthenticationUtils $utils)
-    {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app.homepage');
-        }
-
-        $lastUsername = $utils->getLastUsername();
-        $lastError = $utils->getLastAuthenticationError();
-
-        $form = $this->createForm(LoginType::class, ['username' => $lastUsername]);
-
-        return $this->render('app/login.html.twig', [
-            'form' => $form->createView(),
-            'error' => $lastError
-        ]);
-    }
-
-    /**
      * Logout
      * @Route("/logout", name="app.logout")
      */
