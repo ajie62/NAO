f1c9319508e66ec566e89006cfa2745f97981554
sofyann <sofyann.fahim@gmail.com>
2018-04-25 04:58:21 +0200

    edit users by admin

---
+++ src/Form/EditProfilType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sofyann
+ * Date: 25/04/2018
+ * Time: 03:18
+ */
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class EditProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mail', EmailType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez indiquer une adresse mail.'
+                    ])
+                ]
+            ])
+            ->add('introduction', TextareaType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'max' => 700,
+                        'maxMessage' => 'Votre description ne peut pas faire plus de 700 caractères.'
+                    ])
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('data_class', User::class);
+    }
+}
